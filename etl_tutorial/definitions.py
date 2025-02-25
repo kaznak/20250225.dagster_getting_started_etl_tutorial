88dd5d3aae1f3435ec9c380a7ed7ad075b2333c7
kaznak <kaznak.at.work@ipebble.org>
2025-02-25 11:45:53 +0900

    update

--- etl_tutorial/definitions.py
+++ etl_tutorial/definitions.py
@@ -85,11 +85,56 @@ def sales_data(duckdb: DuckDBResource) -> dg.MaterializeResult:
         )
 
 
+@dg.asset(
+    compute_kind="duckdb",
+    group_name="joins",
+    deps=[sales_data, sales_reps, products],
+)
+def joined_data(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    with duckdb.get_connection() as conn:
+        conn.execute(
+            """
+            create or replace view joined_data as (
+                select
+                    date,
+                    dollar_amount,
+                    customer_name,
+                    quantity,
+                    rep_name,
+                    department,
+                    hire_date,
+                    product_name,
+                    category,
+                    price
+                from sales_data
+                left join sales_reps
+                    on sales_reps.rep_id = sales_data.rep_id
+                left join products
+                    on products.product_id = sales_data.product_id
+            )
+            """
+        )
+
+        preview_query = "select * from joined_data limit 10"
+        preview_df = conn.execute(preview_query).fetchdf()
+
+        row_count = conn.execute("select count(*) from joined_data").fetchone()
+        count = row_count[0] if row_count else 0
+
+        return dg.MaterializeResult(
+            metadata={
+                "row_count": dg.MetadataValue.int(count),
+                "preview": dg.MetadataValue.md(preview_df.to_markdown(index=False)),
+            }
+        )
+
+
 defs = dg.Definitions(
     assets=[
         products,
         sales_reps,
         sales_data,
+        joined_data,
     ],
     resources={"duckdb": DuckDBResource(database="data/mydb.duckdb")},
 )
